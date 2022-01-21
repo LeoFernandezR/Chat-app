d1536c48315268e00a25dcf9b5a16a7d0988a13b
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-01-21 19:44:57 -0300

    Signup component page

---
+++ chat/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+interface Props {}
+
+const SignUp: React.FC<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  )
+}
+
+export default SignUp
