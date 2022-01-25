883dd6e4b200e3f5e30d4b1cc65783c045ae05df
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-01-25 01:10:56 -0300

    socket io implementation with express

--- chat-server/src/app.ts
+++ chat-server/src/app.ts
@@ -1,11 +1,28 @@
 import express from "express"
+import {createServer} from "http"
+import {Server} from "socket.io"
 import config from "./config"
 import cors from "cors"
 import authRoutes from "./routes/auth.routes"
-const app = express()
+import sockets from "./sockets"
+import {authValidation} from "./middlewares/socketMiddleware"
 
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+})
+
+//websocket
+io.use(authValidation)
+io.on("connection", sockets)
+
+//express variables
 app.set("port", config.PORT)
 
+//express middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -13,4 +30,4 @@ app.use(express.urlencoded({extended: false}))
 //routes
 app.use("/api/auth", authRoutes)
 
-export default app
+export {httpServer, app}
