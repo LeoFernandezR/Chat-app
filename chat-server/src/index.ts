6e1cce38b53d3f759edde6895d98c92161bc3420
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-01-25 01:12:35 -0300

    implementation express socket io

--- chat-server/src/index.ts
+++ chat-server/src/index.ts
@@ -1,6 +1,6 @@
-import app from "./app"
+import {httpServer, app} from "./app"
 import "./db"
 
-app.listen(app.get("port"), () => {
+httpServer.listen(app.get("port"), () => {
   console.log(`Listening on port: ${app.get("port")}`)
 })
