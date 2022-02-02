4ec713892fd493eea9f357bea1ef5ae069d5ddf0
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-02-02 17:56:12 -0300

    style: container, background neon

--- chat/src/pages/SignUp.tsx
+++ chat/src/pages/SignUp.tsx
@@ -1,10 +1,22 @@
+import {Icon} from "@iconify/react"
 import React from "react"
+
+import SignUpForm from "../components/forms/SignUpForm"
+
 interface Props {}
 
 const SignUp: React.FC<Props> = ({}) => {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="flex justify-center items-center flex-1">
+      <div className="relative max-w-xl w-full">
+        <div className="absolute -inset-0.5 bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500 blur" />
+        <div className="relative bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500 text-center rounded-md p-6">
+          <h1 className="text-4xl font-medium mb-6">
+            Register for Chatter <Icon inline className="inline" icon="mdi:file-sign" />
+          </h1>
+          <SignUpForm />
+        </div>
+      </div>
     </div>
   )
 }
