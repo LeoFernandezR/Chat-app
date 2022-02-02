1d7f70b1eb05fb97b5688894d382f540b428edba
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-02-02 16:52:51 -0300

    Refactor socket middleware

--- chat-server/src/middlewares/socketMiddleware.ts
+++ chat-server/src/middlewares/socketMiddleware.ts
@@ -4,32 +4,33 @@ import config from "../config"
 import {ExtendedError} from "socket.io/dist/namespace"
 import jwt from "jsonwebtoken"
 
+interface IPayload {
+  id: string
+  iat: number
+  exp: number
+}
+
 const authValidation = async (socket: Socket, next: (err?: ExtendedError | undefined) => void) => {
   const authToken = socket.handshake.auth.token
 
-  if (authToken) {
-    const token = authToken.split(" ")[1]
-    if (token === null) return next(new Error("Not authorized, provide non-null token"))
+  if (!authToken) return next(new Error("No authorized, no token provided"))
 
-    let id: string
+  const token = authToken.split(" ")[1]
+  if (token === null) return next(new Error("Not authorized, provide non-null token"))
 
-    try {
-      const decode = jwt.verify(token, config.ACCESS_TOKEN_SECRET)
-      id = (decode as {id: string; iat: number; exp: number}).id
+  try {
+    const userDecode = jwt.verify(token, config.ACCESS_TOKEN_SECRET) as IPayload
 
-      if (id) {
-        const user = await User.findById(id, {password: 0})
-        if (!user) return next(new Error("Not authorized, invalid user"))
-      }
-      // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      next(new Error(error.message))
-    }
-    //
-    next()
-  } else {
-    next(new Error("No authorized, no token provided"))
+    if (!userDecode.id) return next(new Error("Not authorized, invalid token"))
+
+    const user = await User.findById(userDecode.id, {password: 0})
+    if (!user) return next(new Error("Not authorized, user doesn't exists"))
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    next(new Error(error.message))
   }
+  //
+  next()
 }
 
 export {authValidation}
