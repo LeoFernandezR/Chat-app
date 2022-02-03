bf3fd90ce4f4fd24c1303eca3231ece7d7c44152
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-02-03 17:26:25 -0300

    show password button implementation

--- chat/src/ui/form/TextField.tsx
+++ chat/src/ui/form/TextField.tsx
@@ -1,3 +1,4 @@
+import {Icon} from "@iconify/react"
 import React, {useState} from "react"
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -7,24 +8,36 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 const TextField: React.FC<Props> = ({className, type, password, ...props}) => {
   const [showPassword, setShowPassword] = useState<boolean>(false)
 
+  const buttonIcon = showPassword ? (
+    <Icon inline className="inline" icon="mdi:eye-outline" />
+  ) : (
+    <Icon inline className="inline" icon="mdi:eye-off-outline" />
+  )
+
   const toggleShowPassword = () => {
     setShowPassword(!showPassword)
   }
 
   return (
-    <div>
+    <div className="relative">
       {password ? (
         <>
           <input
-            className={`p-2 px-4 w-full rounded-md bg-stone-700/30 text-white font-light text-xl placeholder:text-white/70 outline-none  ${className}`}
+            className={`relative p-2 px-4 w-full rounded-md bg-stone-700/30 text-white font-light text-xl placeholder:text-white/70 outline-none focus:ring focus:ring-pink-400 transition-all ease-in duration-300  ${className}`}
             type={showPassword ? "text" : "password"}
             {...props}
           />
-          <button onClick={toggleShowPassword} />
+          <button
+            className="absolute top-0 bottom-0 right-4 text-2xl outline-none hover:text-pink-400 focus:text-pink-400 transition-colors ease-in duration-300"
+            type="button"
+            onClick={toggleShowPassword}
+          >
+            {buttonIcon}
+          </button>
         </>
       ) : (
         <input
-          className={`p-2 px-4 w-full rounded-md bg-stone-700/30 text-white font-light text-xl placeholder:text-white/70 outline-none  ${className}`}
+          className={`relative p-2 px-4 w-full rounded-md bg-stone-700/30 text-white font-light text-xl placeholder:text-white/70 outline-none focus:ring focus:ring-pink-400 transition-all ease-in duration-300  ${className}`}
           type={type}
           {...props}
         />
