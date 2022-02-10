8e92b28383ef83d562e85fae46232eae9f96587b
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-02-09 22:57:32 -0300

    feature: login page

--- chat/src/pages/Login.tsx
+++ chat/src/pages/Login.tsx
@@ -1,10 +1,22 @@
+import {Icon} from "@iconify/react"
 import React from "react"
+
+import LoginForm from "../components/forms/LoginForm"
+
 interface Props {}
 
 const Login: React.FC<Props> = ({}) => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center items-center flex-1">
+      <div className="relative max-w-xl w-full">
+        <div className="absolute -inset-0.5 bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500 blur" />
+        <div className="relative bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500 text-center rounded-md p-6">
+          <h1 className="text-4xl font-medium mb-6">
+            Login <Icon inline className="inline" icon="mdi:login" />
+          </h1>
+          <LoginForm />
+        </div>
+      </div>
     </div>
   )
 }
