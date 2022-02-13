d868bb294e54d2c55dd180493f5cea9dc35f502b
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-02-13 03:19:14 -0300

    fix: setting the userId to the request obj

--- chat-server/src/middlewares/authJWT.ts
+++ chat-server/src/middlewares/authJWT.ts
@@ -1,21 +1,26 @@
 import {RequestHandler} from "express"
-import jwt from "jsonwebtoken"
+import jwt, {JwtPayload} from "jsonwebtoken"
 import User from "../models/User"
 import config from "../config"
 
+export interface jwtDecode extends JwtPayload {
+  id: string
+}
+
 const verifyToken: RequestHandler = async (req, res, next) => {
   try {
-    const authHeader = req.headers["authorization"]
+    const authHeader = req.headers["authorization"] as string | undefined
 
     if (authHeader) {
+      console.log(authHeader)
       const token = authHeader.split(" ")[1]
       if (token === null) return res.sendStatus(401)
 
-      jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, userId) => {
+      jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, decode) => {
         if (err) return res.sendStatus(403)
-        req.userId = String(userId)
-      })
 
+        req.userId = (decode as jwtDecode).id
+      })
       const user = await User.findById(req.userId, {password: 0})
       if (!user) return res.status(404).send("User not found")
 
