3c7ddaa4242c20fc4625b490080d3b5522e22cc7
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-02-12 22:16:36 -0300

    fix: content alfways centered

--- chat/src/pages/Home.tsx
+++ chat/src/pages/Home.tsx
@@ -5,18 +5,22 @@ interface Props {}
 
 const Home: React.FC<Props> = ({}) => {
   return (
-    <main className="flex flex-col justify-between items-center">
-      <div className="pt-12 flex flex-col gap-4 justify-center">
-        <h1 className="text-5xl font-bold text-center">
-          <span className="text-pink-400">Welcome</span> to the chat app
-        </h1>
-        <p className="font-light text-lg text-center">
-          Connect with your friends or make new ones through this web chat app <br />
-        </p>
-      </div>
-      <div className="w-96 py-14">
-        <img alt="" className="w-full" src={ChatSVG} />
-      </div>
+    <main className="flex flex-1 items-center justify-center">
+      <article className="flex flex-1  justify-center">
+        <section className="flex flex-col gap-4 justify-center items-center">
+          <div className="flex flex-col gap-2 justify-center py-4">
+            <h1 className="text-6xl font-bold text-center">
+              <span className="text-pink-400">Welcome</span> to the chat app
+            </h1>
+            <p className="font-light text-lg text-center">
+              Connect with your friends or make new ones through this web chat app <br />
+            </p>
+          </div>
+          <div className="max-w-md w-full">
+            <img alt="" className="w-full" src={ChatSVG} />
+          </div>
+        </section>
+      </article>
     </main>
   )
 }
