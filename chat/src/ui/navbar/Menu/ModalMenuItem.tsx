05ec268765c97441f8ac2478c2e2d45fe717d877
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-02-25 17:55:07 -0300

    feature: conditional render button or link

--- chat/src/ui/navbar/Menu/ModalMenuItem.tsx
+++ chat/src/ui/navbar/Menu/ModalMenuItem.tsx
@@ -1,17 +1,32 @@
 import React from "react"
 import {Link} from "react-router-dom"
+
 interface Props {
   title: string
-  url: string
+  url?: string
+  action: "button" | "link"
+  onClick: VoidFunction
 }
 
-const ModalMenuItem: React.FC<Props> = ({title, url}) => {
+const ModalMenuItem: React.FC<Props> = ({title, url = "/", action, onClick}) => {
+  const isLink = action === "link"
+
   return (
-    <Link to={url}>
-      <li className="w-full py-4 mb-[3px] last:mb-0 bg-stone-800 group hover:bg-transparent transition-colors duration-300 cursor-pointer ease-in">
-        {title}
-      </li>
-    </Link>
+    <>
+      {isLink ? (
+        <Link to={url} onClick={onClick}>
+          <li className="w-full py-4 mb-[3px] last:mb-0 bg-stone-800 group hover:bg-transparent transition-colors duration-300 cursor-pointer ease-in block">
+            {title}
+          </li>
+        </Link>
+      ) : (
+        <li className="w-full py-4 mb-[3px] last:mb-0 bg-stone-800 group hover:bg-transparent transition-colors duration-300 cursor-pointer ease-in block">
+          <button className="w-full" onClick={onClick}>
+            {title}
+          </button>
+        </li>
+      )}
+    </>
   )
 }
 
