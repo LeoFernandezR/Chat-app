89491d2464491bcac33316cfadca388769587d5b
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-02-25 17:57:14 -0300

    feat: handle menu when user is logged in

--- chat/src/ui/navbar/Menu/Menu.tsx
+++ chat/src/ui/navbar/Menu/Menu.tsx
@@ -1,38 +1,56 @@
 import React from "react"
 import {Link} from "react-router-dom"
 
-import menuOptions from "./MenuOptions.json"
-import ModalMenu from "./ModalMenu"
+import useAuthContext from "../../../hooks/useAuthContext"
 
-export interface Option {
-  title: string
-  url: string
-}
+import ModalMenu from "./ModalMenu"
 
 interface Props {}
 
 const Menu: React.FC<Props> = ({}) => {
+  const {loggedIn, user, logout} = useAuthContext()
+
   return (
     <div>
-      <ModalMenu menuOptions={menuOptions} />
+      <ModalMenu />
       <nav className="hidden sm:block">
-        <ul className="flex gap-2 text-lg font-medium">
-          <li>
-            <Link
-              className="px-4 py-2 hover:text-pink-400 transition-colors duration-300"
-              to="/login"
-            >
-              Log in
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="px-4 py-2 bg-pink-400 rounded-md hover:bg-pink-500 transition-colors duration-300 ease-in"
-              to="/signup"
-            >
-              Sign up
-            </Link>
-          </li>
+        <ul className="flex gap-2 text-xl items-center">
+          {loggedIn ? (
+            <>
+              <li>
+                <p className="mr-2">
+                  Hello, <span className="text-pink-300 font-medium ">{user?.username}</span>👋
+                </p>
+              </li>
+              <li>
+                <button
+                  className="px-4 py-1 bg-pink-400 rounded-md hover:bg-pink-500 transition-colors duration-300 ease-in "
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link
+                  className="px-4 py-1 hover:text-pink-400 transition-colors duration-300"
+                  to="/login"
+                >
+                  Log in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="block px-4 py-1 bg-pink-400 rounded-md hover:bg-pink-500 transition-colors duration-300 ease-in"
+                  to="/signup"
+                >
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
