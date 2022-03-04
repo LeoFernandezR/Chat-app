8286e14f9bececcfb14943fc3f625040c15c835b
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-03-04 16:49:44 -0300

    fix: decoded error not returning the verifyToken func

--- chat-server/src/middlewares/authJWT.ts
+++ chat-server/src/middlewares/authJWT.ts
@@ -8,26 +8,27 @@ export interface jwtDecode extends JwtPayload {
 }
 
 const verifyToken: RequestHandler = async (req, res, next) => {
-  try {
-    const authHeader = req.headers["authorization"] as string | undefined
+  const authHeader = req.headers["authorization"] as string | undefined
 
-    if (authHeader) {
-      console.log(authHeader)
-      const token = authHeader.split(" ")[1]
-      if (token === null) return res.sendStatus(401)
+  if (authHeader) {
+    const token = authHeader.split(" ")[1]
+    if (token === null) return res.sendStatus(401)
 
-      jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, decode) => {
-        if (err) return res.sendStatus(403)
+    try {
+      const decoded = jwt.verify(token, config.ACCESS_TOKEN_SECRET)
 
-        req.userId = (decode as jwtDecode).id
-      })
+      req.userId = (decoded as jwtDecode).id
+    } catch (error) {
+      return res.sendStatus(403)
+    }
+
+    try {
       const user = await User.findById(req.userId, {password: 0})
       if (!user) return res.status(404).send("User not found")
-
-      next()
+    } catch (err) {
+      return res.sendStatus(500)
     }
-  } catch (err) {
-    return res.sendStatus(500)
+    next()
   }
 }
 
