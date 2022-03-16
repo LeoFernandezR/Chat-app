41a5ef7c071c79623efe4acf99c022c53d33cf3b
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-03-16 17:08:46 -0300

    feat: new logo

--- chat/src/components/commons/navbar/Navbar.tsx
+++ chat/src/components/commons/navbar/Navbar.tsx
@@ -2,20 +2,24 @@ import React from "react"
 import {InlineIcon} from "@iconify/react"
 import {Link} from "react-router-dom"
 
+import ChatterLogo from "../../../assets/logo/logo.png"
+
 import Menu from "./Menu/Menu"
 interface Props {}
 
 const Navbar: React.FC<Props> = ({}) => {
   return (
-    <header className=" bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 shadow relative w-full">
+    <header className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 shadow relative w-full">
       <div className="absolute bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 blur w-full h-full" />
 
-      <div className="flex items-center justify-between px-6 py-2 relative">
+      <div className="flex items-center justify-between px-6 py-4 relative">
         <Link to="/">
-          <h1 className="text-2xl sm:text-3xl py-2 pr-4">
-            <InlineIcon className="inline mr-2 text-blue-400" icon="mdi:react" />
-            Chatter
-          </h1>
+          <div className="flex items-stretch">
+            <div className="w-10 mr-1">
+              <img className="w-full" src={ChatterLogo} />
+            </div>
+            <h1 className="text-3xl font-medium">Chatter </h1>
+          </div>
         </Link>
         <Menu />
       </div>
