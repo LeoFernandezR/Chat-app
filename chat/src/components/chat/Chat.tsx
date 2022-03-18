947d747d9aa24646bc512c3684a382461616d8c7
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-03-17 22:09:26 -0300

    feat: extract aside,  feat: divide sections

--- chat/src/components/chat/Chat.tsx
+++ chat/src/components/chat/Chat.tsx
@@ -1,20 +1,37 @@
 import React, {useState} from "react"
+
+import ChatAside from "./ChatAside"
+import NewRoomModal from "./NewRoomModal"
+
 interface Props {}
 
 export const Chat: React.FC<Props> = ({}) => {
-  const [rooms, setRooms] = useState(Array(7).fill("lorem ipsum"))
+  const [rooms, setRooms] = useState(Array(0).fill("lorem ipsum"))
+  const [showNewRoomModal, setShowNewRoomModal] = useState(false)
+
+  const addNewRoom = (newRoom: string) => {
+    setRooms((prevRooms) => [...prevRooms, newRoom])
+    closeNewRoomModal()
+  }
+
+  const closeNewRoomModal = () => setShowNewRoomModal(false)
+
+  const openNewRoomModal = () => setShowNewRoomModal(true)
 
   return (
-    <div className="min-h-screen from-pink-500 via-purple-500 to-indigo-500 bg-gradient-to-br">
-      <aside className="p-4 flex flex-col gap-2 border-r flex-[0.15] min-w-fit">
-        <h1 className="text-lg">Chat Rooms </h1>
-        <ul className="">
-          {rooms.map((room) => (
-            <li key={room}># {room}</li>
-          ))}
-        </ul>
-      </aside>
-      <div className="flex-[0.85]" />
-    </div>
+    <>
+      <div className="flex min-h-screen from-pink-500 via-purple-500 to-indigo-500 bg-gradient-to-br text-white">
+        <ChatAside openNewRoomModal={openNewRoomModal} rooms={rooms} />
+        <div className="flex-[0.85] flex flex-col">
+          <div className="flex-[0.90]">messages</div>
+          <div className="w-full flex-[0.10] p-4">sendmessage</div>
+        </div>
+      </div>
+      <NewRoomModal
+        addNewRoom={addNewRoom}
+        closeNewRoomModal={closeNewRoomModal}
+        showNewRoomModal={showNewRoomModal}
+      />
+    </>
   )
 }
