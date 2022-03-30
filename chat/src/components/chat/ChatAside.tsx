c3ce5a743421af81e538629ca1ef66a0aee713ab
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-03-30 01:22:17 -0300

    feat: custom scrollbar, fix: responsive height

--- chat/src/components/chat/ChatAside.tsx
+++ chat/src/components/chat/ChatAside.tsx
@@ -1,41 +1,74 @@
-import React from "react"
+import React, {useLayoutEffect} from "react"
 import {Icon} from "@iconify/react"
+import {useRef} from "react"
 
 import ChatterLogo from "../../assets/logo/logo.png"
 import useAuthContext from "../../hooks/useAuthContext"
 
+import {Room} from "./Chat"
+
 interface Props {
-  rooms: string[]
+  rooms: Room[]
   openNewRoomModal: VoidFunction
+  handleRoomSelect: (room: Room) => void
 }
 
-const ChatAside: React.FC<Props> = ({rooms, openNewRoomModal}) => {
+const ChatAside: React.FC<Props> = ({rooms, openNewRoomModal, handleRoomSelect}) => {
   const {user, logout} = useAuthContext()
+  const headerRef = useRef<HTMLDivElement | null>(null)
+  const userMenuRef = useRef<HTMLDivElement | null>(null)
+  const roomListRef = useRef<HTMLUListElement | null>(null)
+
+  const headerHeight = headerRef.current?.offsetHeight ?? 0
+  const userMenuHeight = userMenuRef.current?.offsetHeight ?? 0
+
+  const maxRoomList = window.innerHeight - (headerHeight + userMenuHeight)
+
+  useLayoutEffect(() => {
+    if (!roomListRef.current) return
+
+    window.addEventListener("resize", () => {
+      if (!roomListRef.current || !headerRef.current || !userMenuRef.current) {
+        return
+      }
+
+      roomListRef.current.style.maxHeight = `${
+        window.innerHeight - (headerRef.current.offsetHeight + userMenuRef.current.offsetHeight)
+      }px`
+    })
+  }, [])
 
   return (
-    <aside className="flex flex-col flex-[0.15] min-w-fit shadow-xl">
-      <div className="flex-1 p-4">
-        <header className="flex items-stretch justify-center mb-6">
-          <div className="w-10 mr-1">
-            <img className="w-full" src={ChatterLogo} />
+    <>
+      <div className="flex-1">
+        <header ref={headerRef} className="flex flex-col gap-2 px-4 pt-4 pb-2">
+          <div className="flex justify-center">
+            <div className="w-10 mr-1">
+              <img className="w-full" src={ChatterLogo} />
+            </div>
+            <h1 className="text-3xl font-medium">Chatter </h1>
           </div>
-          <h1 className="text-3xl font-medium">Chatter </h1>
-        </header>
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between">
+          <div className="flex justify-between ">
             <h3 className="text-xl font-medium">Chat Rooms </h3>
             <button className="inline text-right" onClick={openNewRoomModal}>
               <Icon icon="fluent:add-12-filled" inline={true} />
             </button>
           </div>
-          <ul className="pl-3 flex flex-col gap-1 overflow-y-auto max-h-[48.5rem]">
-            {rooms.map((room) => (
-              <li key={room}># {room}</li>
-            ))}
-          </ul>
-        </div>
+        </header>
+
+        <ul
+          ref={roomListRef}
+          className="flex flex-col gap-1 overflow-y-auto px-4"
+          style={{maxHeight: maxRoomList}}
+        >
+          {rooms.map((room) => (
+            <li key={room.id} className="cursor-pointer" onClick={() => handleRoomSelect(room)}>
+              # {room.name}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="flex justify-between items-center p-4 gap-4">
+      <div ref={userMenuRef} className="flex justify-between items-center p-4 gap-4">
         <h4 className="text-lg">
           Hello <span className="font-bold">{user?.username}</span> 👋
         </h4>
@@ -46,7 +79,7 @@ const ChatAside: React.FC<Props> = ({rooms, openNewRoomModal}) => {
           <Icon className="inline" icon="tabler:logout" inline={true} />
         </button>
       </div>
-    </aside>
+    </>
   )
 }
 
