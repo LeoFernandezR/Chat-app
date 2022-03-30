746527e197fb8625b025a50472aff4b8aa7c750d
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2022-03-30 01:23:53 -0300

    feat: room description

--- chat/src/components/chat/NewRoomModal.tsx
+++ chat/src/components/chat/NewRoomModal.tsx
@@ -4,12 +4,13 @@ import {motion, AnimatePresence} from "framer-motion"
 import TextField from "../commons/form/TextField"
 interface Props {
   showNewRoomModal: boolean
-  addNewRoom: (newRoom: string) => void
+  addNewRoom: (newRoom: {roomName: string; roomDescription: string}) => void
   closeNewRoomModal: VoidFunction
 }
 
 const NewRoomModal: React.FC<Props> = ({showNewRoomModal, addNewRoom, closeNewRoomModal}) => {
   const [roomName, setRoomName] = useState("")
+  const [roomDescription, setRoomDescription] = useState("")
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   useEffect(() => {
@@ -21,7 +22,7 @@ const NewRoomModal: React.FC<Props> = ({showNewRoomModal, addNewRoom, closeNewRo
   const handleRoomCreation = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    addNewRoom(roomName)
+    addNewRoom({roomName, roomDescription})
     setRoomName("")
   }
 
@@ -38,18 +39,23 @@ const NewRoomModal: React.FC<Props> = ({showNewRoomModal, addNewRoom, closeNewRo
         >
           <div className="bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500 max-w-lg w-full p-6 rounded-md">
             <form className="flex flex-col gap-4" onSubmit={handleRoomCreation}>
-              <h1 className="text-3xl text-center">Create new room</h1>
+              <h1 className="text-3xl text-center">New Room</h1>
               <TextField
                 ref={inputRef}
                 placeholder="Room name"
                 value={roomName}
                 onChange={(e) => setRoomName(e.target.value)}
               />
+              <TextField
+                placeholder="Room description (optional)"
+                value={roomDescription}
+                onChange={(e) => setRoomDescription(e.target.value)}
+              />
               <nav>
                 <ul className="flex justify-between gap-4">
                   <li className="w-full">
                     <button
-                      className="w-full px-4 py-2 text-lg text-center bg-teal-400 rounded-md hover:bg-teal-500 transition-color duration-300 ease-in font-medium"
+                      className="w-full px-4 py-2 text-lg text-center bg-teal-500 rounded-md hover:bg-teal-600 transition-color duration-300 ease-in font-medium"
                       type="submit"
                     >
                       Create
@@ -57,7 +63,7 @@ const NewRoomModal: React.FC<Props> = ({showNewRoomModal, addNewRoom, closeNewRo
                   </li>
                   <li className="w-full">
                     <button
-                      className="w-full px-4 py-2 text-lg text-center bg-rose-500 rounded-md hover:bg-rose-600 transition-color duration-300 ease-in font-medium"
+                      className="w-full px-4 py-2 text-lg text-center bg-rose-500 rounded-md hover:bg-rose-700 transition-color duration-300 ease-in font-medium"
                       type="button"
                       onClick={closeNewRoomModal}
                     >
